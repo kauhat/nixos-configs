0a9f8c7bf41abf079cb3c205914ff5d70850167b
Jack Fletcher <jackowenfletcher@gmail.com>
2024-11-10 14:47:29 +0000

    Add mobile SSH key

--- users/jack.nix
+++ users/jack.nix
@@ -1,5 +1,4 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+# This is your home-manager configuration file.
 {
   inputs,
   outputs,
@@ -9,14 +8,18 @@
   ...
 }: {
   config = {
+    # programs.zsh.enable = true;
+
     # Add an admin user
     users.users.jack = {
       isNormalUser = true;
       description = "It's me, Jack!";
       extraGroups = ["networkmanager" "wheel"];
+      # shell = pkgs.zsh;
 
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPO4HOkIIuHnAhukRppCJ4lWpR/XfT77cO4b9fFVz2cu"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPO4HOkIIuHnAhukRppCJ4lWpR/XfT77cO4b9fFVz2cu jack@jackspc-bazzite"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICukXQCZucPptRM5ciQCEY5/6qvMedGymAtI5yOVCl3F jack@mobile"
       ];
     };
   };
