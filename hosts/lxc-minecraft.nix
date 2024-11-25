9e5adba4271e5df90c716add9c17c31e321d3b71
Jack Fletcher <jackowenfletcher@gmail.com>
2024-11-25 01:00:39 +0000

    Update minecraft config

--- hosts/lxc-minecraft.nix
+++ hosts/lxc-minecraft.nix
@@ -6,7 +6,9 @@
   lib,
   system,
   ...
-}: {
+}: let
+  networkName = "minecraft-bridge";
+in {
   imports = [
     ./base-insecure.nix
     ./base-lxc-tailscale.nix
@@ -20,10 +22,9 @@
 
     # Create the Cloudflare API key file
 
-    # Update Cloudflare service DNS record.
+    # Update Cloudflare service DNS record...
     services.cloudflare-dyndns = {
       enable = true;
-      apiTokenFile = "/etc/secrets/cloudflare_api_key";
       domains = [
         "minecraft.services.kauh.at"
       ];
@@ -32,6 +33,30 @@
     # Minecraft containers...
     virtualisation.oci-containers.backend = "podman";
     virtualisation.oci-containers.containers = {
+      "gate" = {
+        image = "ghcr.io/minekube/gate:latest";
+        autoStart = true;
+        ports = ["25555:25555/tcp"];
+        # extraOptions = ["--network=${networkName}"];
+        volumes = [
+          "/etc/gate/config.yml:/config.yml"
+        ];
+      };
+
+
+      "minecraft_vanilla" = {
+        image = "itzg/minecraft-server:stable";
+        autoStart = true;
+        hostname = "minecraft-vanilla";
+        ports = ["25545:25565"];
+        # extraOptions = ["--network=${networkName}"];
+        environment = {
+          EULA = "true";
+          MEMORY = "2G";
+          MOTD = "Vanilla";
+        };
+      };
+
       "minecraft_e2e" = {
         image = "itzg/minecraft-server:java8-multiarch";
         autoStart = true;
@@ -49,5 +74,22 @@
         };
       };
     };
+
+    # systemd.services.init-minecraft-network = {
+    #   description = "Create a network bridge for minecraft containers.";
+    #   after = ["network.target"];
+    #   wantedBy = ["multi-user.target"];
+    #   serviceConfig.Type = "oneshot";
+    #   script = ''
+    #     # Put a true at the end to prevent getting non-zero return code, which will
+    #     # crash the whole service.
+    #     check=$(${pkgs.podman}/bin/podman network ls | grep "${networkName}" || true)
+    #     if [ -z "$check" ]; then
+    #       ${pkgs.podman}/bin/podman network create ${networkName}
+    #     else
+    #       echo "${networkName} already exists"
+    #      fi
+    #   '';
+    # };
   };
 }
